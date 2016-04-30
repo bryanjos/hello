c34ad9bbf27dd0124b0fdf3ef441affd948f0597
Keith Salisbury <keithsalisbury@gmail.com>
2016-04-30 01:27:10 +0100

    Introduct Phaser
    This commit introduces the Phaser JS library and create a simple game
    using Elixirscript. This version creates one simple state for phaser to
    work with. Next step is to figure out how to create states as modules in
    Elixirscript and set them up in the game.

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -11,7 +11,7 @@
 //
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
-import "phoenix_html";
+import "phoenix_html"
 
 // Import local files
 //
@@ -19,6 +19,6 @@ import "phoenix_html";
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
-import Hello from "./Elixir.Hello";
+import Game from "./Elixir.Game"
 
-Hello.init();
+Game.init()
