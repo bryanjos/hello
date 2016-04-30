33ea0ef81865e6c046d9a167db6eb4dc1390bb38
Keith Salisbury <keithsalisbury@gmail.com>
2016-04-30 01:27:10 +0100

    wip

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -19,6 +19,8 @@ import "phoenix_html";
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
-import Hello from "./Elixir.Hello";
+// import Hello from "./Elixir.Hello";
+import Game from "./Elixir.Game";
 
-Hello.hello();
+// Hello.hello();
+Game.init();
